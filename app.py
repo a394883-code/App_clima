30245f25be9423f48d2d61a56bc02ce15d5a9261
a394883-code <a394883@uach.mx>
2026-09-15 01:20:22 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Control de acceso")
+
+edad = st.number_imput("Edad:", min_value = 0, value = 18)
+
+tiene_identificación = st.checkbox("Tiene identificación")
+
+if edad >= 18 and tiene_identificación:
+  st.write("Puede ingresar.")
+else:
+  st.write("No puede ingresar.")
